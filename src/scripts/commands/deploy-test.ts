b68331c8fee9055020a17c7f8df0916c2ede19d4
Isaia Riva <isaia.riva@finconsgroup.com>
2025-01-29 09:26:06 +0100

    version: 2.0.6

--- src/scripts/commands/deploy-test.ts
+++ src/scripts/commands/deploy-test.ts
@@ -1,16 +1,17 @@
 import { Command } from 'commander';
 import { execSync } from 'child_process';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export const deployTest = new Command('deploy-test');
 
 deployTest
-	.name('deploy-test')
-	.description('Run the deploy-test.js script')
-	.action(() => {
-		const scriptPath = path.resolve(
-			__dirname,
-			'../../../data-on-fire/scripts/deploy-test.js'
-		);
-		execSync(`node ${scriptPath}`, { stdio: 'inherit' });
-	});
+  .name('deploy-test')
+  .description('Run the deploy-test.js script')
+  .action(() => {
+    const scriptPath = path.resolve(__dirname, '../../../data-on-fire/scripts/deploy-test.js');
+    execSync(`node ${scriptPath}`, { stdio: 'inherit' });
+  });
