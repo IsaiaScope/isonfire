e5e875853e614ce47e5477fe330a7d31240491a0
Isaia Riva <isaia.riva@finconsgroup.com>
2025-01-29 13:10:13 +0100

    version: 2.0.12

--- src/scripts/commands/deploy-test.ts
+++ src/scripts/commands/deploy-test.ts
@@ -12,6 +12,6 @@ deployTest
   .name('deploy-test')
   .description('Run the deploy-test.js script')
   .action(() => {
-    const scriptPath = path.resolve(__dirname, './data-on-fire/scripts/commands/deploy-test.js');
+    const scriptPath = path.resolve(__dirname, '../data-on-fire/scripts/commands/deploy-test.js');
     execSync(`node ${scriptPath}`, { stdio: 'inherit' });
   });
